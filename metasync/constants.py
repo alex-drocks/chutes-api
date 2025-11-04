cb8f7778369c96585a8a046622b57ffab22072e4
Jon Durbin <jon@jondurbin.com>
2025-11-04 16:16:35 +0000

    Fix private instance query (ensure active instances accounted for).

--- metasync/constants.py
+++ metasync/constants.py
@@ -145,11 +145,15 @@ WITH billed_instances AS (
     FROM instance_audit ia
     WHERE ia.billed_to IS NOT NULL
       AND ia.activated_at IS NOT NULL
-      AND (ia.deletion_reason in (
-        'job has been terminated due to insufficient user balance',
-        'user-defined/private chute instance has not been used since shutdown_after_seconds',
-        'user has zero/negative balance (private chute)'
-      ) or ia.deletion_reason like '%has an old version%')
+      AND (
+        ia.deletion_reason in (
+          'job has been terminated due to insufficient user balance',
+          'user-defined/private chute instance has not been used since shutdown_after_seconds',
+          'user has zero/negative balance (private chute)'
+        )
+        OR ia.deletion_reason LIKE '%has an old version%'
+        OR ia.deleted_at IS NULL
+      )
       AND (ia.stop_billing_at IS NULL OR ia.stop_billing_at >= now() - interval '{interval}')
 ),
 
