8ba52d66eadff9c95a9e3fb1f4817013230dabc6
Jon Durbin <jon@jondurbin.com>
2025-11-20 21:14:19 +0000

    Revert "Allow invoiced users to have quotas."
    This reverts commit b494697d04ed50d19daf998eb8a459d3d6b87111.

--- api/user/router.py
+++ api/user/router.py
@@ -513,7 +513,9 @@ async def my_quotas(
     """
     Load quotas for the current user.
     """
-    if current_user.has_role(Permissioning.free_account):
+    if current_user.has_role(Permissioning.free_account) or current_user.has_role(
+        Permissioning.invoice_billing
+    ):
         return {}
     quotas = (
         (
@@ -642,7 +644,9 @@ async def chute_quota_usage(
     """
     Check the current quota usage for a chute.
     """
-    if current_user.has_role(Permissioning.free_account):
+    if current_user.has_role(Permissioning.free_account) or current_user.has_role(
+        Permissioning.invoice_billing
+    ):
         return {"quota": "unlimited", "used": 0}
     quota = await InvocationQuota.get(current_user.user_id, chute_id)
     key = await InvocationQuota.quota_key(current_user.user_id, chute_id)
