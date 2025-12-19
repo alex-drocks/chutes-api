4ecfeb761179fa9cc528cf6fa8be89ff1c5ee688
Jon Durbin <jon@jondurbin.com>
2025-12-19 14:43:21 +0000

    HF repo info proxy endpoint, scaling updates.

--- chute_autoscaler.py
+++ chute_autoscaler.py
@@ -52,17 +52,18 @@ PRICE_COMPATIBILITY_THRESHOLD = 0.67
 # Higher min instance counts for some chutes...
 LIMIT_OVERRIDES = {}
 FAILSAFE = {
-    "0d7184a2-32a3-53e0-9607-058c37edaab5": 36,
-    "579ca543-dda4-51d0-83ef-5667d1a5ed5f": 16,
+    "0d7184a2-32a3-53e0-9607-058c37edaab5": 26,
+    "14a91d88-d6d6-5046-aaf4-eb3ad96b7247": 20,
     "4fa0c7f5-82f7-59d1-8996-661bb778893d": 15,
-    "0df3133d-c477-56d2-b4db-f2093bb150a1": 15,
+    "579ca543-dda4-51d0-83ef-5667d1a5ed5f": 13,
+    "0df3133d-c477-56d2-b4db-f2093bb150a1": 12,
     "d711f181-5b21-5169-a011-ccb472a1604f": 10,
-    "39d75699-957f-571f-8737-f2c72819d3e8": 8,
-    "8f3bb827-b9e6-5487-88bc-ee8f0c6f5810": 8,
-    "3048cf8d-67de-5a6d-9fdd-18ac9c560c05": 5,
+    "8d008c10-60d3-51e8-9272-c428ed6ff576": 8,
+    "02636d63-c996-5779-a0a2-25712469a7ca": 8,
+    "b2b7a64c-b203-5a5f-8982-a9c5cc12058c": 8,
+    "8f3bb827-b9e6-5487-88bc-ee8f0c6f5810": 6,
     "4bbc44e9-6bfc-5e21-a91d-129bff2fb6d4": 5,
     "ae3b9d04-28fa-543a-9276-290da772dc23": 5,
-    "8d008c10-60d3-51e8-9272-c428ed6ff576": 5,
     "aef797d4-f375-5beb-9986-3ad245947469": 5,
     "689d2caa-01c1-5de1-ba69-39c5398be0c6": 5,
 }
