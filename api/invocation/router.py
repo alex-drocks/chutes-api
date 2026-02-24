1a1dff14b6c724f9760b041b64ed1a72c27460f3
Jon Durbin <jon@jondurbin.com>
2026-02-24 02:13:50 +0000

    Re-enable 3.2-exp temp route.

--- api/invocation/router.py
+++ api/invocation/router.py
@@ -908,14 +908,14 @@ async def hostname_invocation(
             payload["model"] = "XiaomiMiMo/MiMo-V2-Flash-TEE"
 
         # XXX in case needed again.
-        #elif (
-        #    model in ("deepseek-ai/DeepSeek-V3.2-Exp", "deepseek-ai/DeepSeek-V3.2-Exp:THINKING")
-        #    and random.random() <= 0.5
-        #):
-        #    if model.endswith("THINKING"):
-        #        payload["model"] = "deepseek-ai/DeepSeek-V3.2-Exp-TEE:THINKING"
-        #    else:
-        #        payload["model"] = "deepseek-ai/DeepSeek-V3.2-Exp-TEE"
+        elif (
+            model in ("deepseek-ai/DeepSeek-V3.2-Exp", "deepseek-ai/DeepSeek-V3.2-Exp:THINKING")
+            and random.random() <= 0.6
+        ):
+            if model.endswith("THINKING"):
+                payload["model"] = "deepseek-ai/DeepSeek-V3.2-Exp-TEE:THINKING"
+            else:
+                payload["model"] = "deepseek-ai/DeepSeek-V3.2-Exp-TEE"
 
         # No file support currently.
         if isinstance(payload.get("messages"), list):
